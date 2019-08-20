601b2794596d9fb1cb580bc0ba4466cdc7e4ed52
Vijay <842644405@qq.com>
2019-08-20 13:02:39 +0800

    aaa

--- src/test/TestExtends.java
+++ src/test/TestExtends.java
@@ -5,6 +5,7 @@ import thread.Extends;
 public class TestExtends {
 	public static void main(String[] args) {
 		Extends e = new Extends();
+		//aaa
 		e.start();
 		for (int i = 0; i < 200; i++) {
 			System.out.println("main...");
